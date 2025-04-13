32f1bbe8e2173d498ea7493c7935c02ea6281ec7
Arun Tiwari <itiwariarun438@gmail.com>
2025-04-13 21:05:39 +0530

    fix: issues

--- components/Pattern.tsx
+++ components/Pattern.tsx
@@ -10,17 +10,17 @@ import {
 import Image from "next/image";
 import { motion } from "motion/react";
 import { PlaceHolderTextArea } from "@utils/constants";
+import { CodeBlock } from "@components/CodeSection";
 
 export function CreatePattern() {
-
   return (
-    <div className="py-40  flex items-center justify-center">
+    <div className="flex items-center justify-center py-40">
       <Modal>
-        <ModalTrigger className="bg-transparent dark:text-black text-white flex justify-center group/modal-btn">
-          <span className="group-hover/modal-btn:translate-x-40 text-center transition duration-500">
+        <ModalTrigger className="flex justify-center text-white bg-transparent dark:text-black group/modal-btn">
+          <span className="text-center transition duration-500 group-hover/modal-btn:translate-x-40">
             Pattern
           </span>
-          <div className="-translate-x-40 group-hover/modal-btn:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500 text-white z-20">
+          <div className="absolute inset-0 z-20 flex items-center justify-center text-white transition duration-500 -translate-x-40 group-hover/modal-btn:translate-x-0">
             ✈️
           </div>
         </ModalTrigger>
@@ -28,13 +28,13 @@ export function CreatePattern() {
           <ModalContent>
             <h2>Create New Blog By Following This Pattern</h2>
             <CodeBlock
-                        language={JSON}
-                        highlightLines={[9, 13, 14, 18]}
-                        code={PlaceHolderTextArea}
-                      />
+              language={JSON}
+              highlightLines={[9, 13, 14, 18]}
+              code={PlaceHolderTextArea}
+            />
           </ModalContent>
           <ModalFooter className="gap-4">
-            <button className="px-2 py-1 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300 rounded-md text-sm w-28">
+            <button className="px-2 py-1 text-sm text-black bg-gray-200 border border-gray-300 rounded-md dark:bg-black dark:border-black dark:text-white w-28">
               Close
             </button>
           </ModalFooter>
